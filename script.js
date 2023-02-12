163efe0da21109f87998fbcb811a6c0c51d72628
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-02-12 19:15:29 +0530

    grid-border

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let click = true;
-let color = 'Black';
+let color = 'Multicolor';
+const board = document.querySelector('.board')
 
 function changeSize(input){
     populateBoard(input)
 }
 
+populateBoard(5)
+
 function populateBoard(size)
 {
 const board = document.querySelector('.board')
-let squares = board.querySelectorAll('div')
-squares.forEach(square=>square.remove())
+// let squares = board.querySelectorAll('div')
+// squares.forEach(square=>square.remove())
+board.innerHTML = ""
 board.style.gridTemplateColumns = `repeat(${size},1fr)`
 board.style.gridTemplateRows = `repeat(${size},1fr)`
 
@@ -17,14 +21,11 @@ for (let i=0;i<size*size;i++)
 {
     let square = document.createElement("div")
     square.addEventListener('mouseover',squareColor)
-    square.style.backgroundColor = "unset";
+    // square.style.backgroundColor = "unset";
     board.insertAdjacentElement("beforeend",square)
     document.querySelector('.value').innerText=`${size}*${size} squares`;
-    let innerTemp=`<h2>Hover over Me</h2>`;
 }
 }
-populateBoard(5)
-
 
 function squareColor(){
     
@@ -43,7 +44,7 @@ function squareColor(){
             board.style.border=`1px solid rgb(${t1},${t2},${t3})`;
         }
     
-        else if(color==='unset')
+        if(color==='unset')
         {
          this.style.backgroundColor = color;
          document.querySelector('.color-code').innerHTML='Erase';
@@ -85,6 +86,10 @@ function reset(){
     const board = document.querySelector('.board')
     let squares = board.querySelectorAll('div')
     squares.forEach(square=>square.remove())
+}
+
+function grid(){
+    board.classList.toggle("border")
 }
 
 function getRandomValue() {
